e37a785373fb2f523a03194677b56ef4881a96a5
andrii-kalyniuk <v13urnum@gmail.com>
2020-01-16 01:26:24 +0200

    func for extending playground, some new zero generation pattern  added

--- life.py
+++ life.py
@@ -2,9 +2,12 @@ import os
 import time
 from random import randrange
 
-from generation_zero_patterns import (LWSS_raw,
+from generation_zero_patterns import (lwss_raw,
                                       pulsar_raw,
-                                      glider_gun_raw)
+                                      glider_gun_raw,
+                                      fuse_raw,
+                                      glider_raw, python_raw,
+                                      ff_force_field_raw)
 
 
 def clear_screen():
@@ -22,10 +25,6 @@ def find_neighborhood(row, column, gen, ignore_borders=True):
     Finds neighbors for the specified cell in an array at a distance of
     one cell and its sum
     """
-    # done_todo: loop the borders of the matrix
-    #   fixed in find_neighborhood_unlim()
-    # done_todo: rename like find_neighborhood_with_boarders or just add
-    #   parameter ignore_boarders=True
     neighborhood = []
     steps = (
         (1, 0), (-1, 0), (0, 1), (0, -1),
@@ -45,69 +44,42 @@ def find_neighborhood(row, column, gen, ignore_borders=True):
     return (row, column), neighborhood, sum(neighborhood)
 
 
-def find_neighborhood_unlim(row, column, gen):
-    """
-    Finds neighbors for the specified cell in an array at a distance of
-    one cell neglecting the boundaries of the array
-    """
-    neighborhood = []
-    steps = (
-        (1, 0), (-1, 0), (0, 1), (0, -1),
-        (1, 1), (-1, 1), (-1, -1), (1, -1)
-    )
-    for row_step, column_step in steps:
-        if row + row_step >= len(gen):
-            row_step = -len(gen) + 1
-        if column + column_step >= len(gen[row]):
-            column_step = -len(gen[row]) + 1
-        neighborhood.append(gen[row + row_step][column + column_step])
-    return (row, column), neighborhood, sum(neighborhood)
-
-
 def next_generation(gen, ignore_borders=True):
     """
     Calculates the next generation of cells based on the state of the
     current generation
     """
-    # gen_next = gen[:]  # not work
-    # gen_next = list(gen)  # not work
     gen_next = []
     for row in range(len(gen)):
-        gen_next.append(gen[row][:])  # it's work
+        gen_next.append(gen[row][:])
         for column in range(len(gen[row])):
-            # print(find_neighborhood(row, column, gen))
-            # life_count = find_neighborhood_unlim(row, column, gen)[2]
             life_count = find_neighborhood(
                 row, column, gen, ignore_borders=ignore_borders)[2]
-            # TODO: refactor ifs
-            if gen[row][column] == 1 and life_count in (2, 3):
-                # stay alive
-                pass
             if gen[row][column] == 0 and life_count == 3:
                 # born
                 gen_next[row][column] = 1
             if gen[row][column] == 1 and (life_count <= 1 or life_count >= 4):
                 # die
                 gen_next[row][column] = 0
-        # print('---now--->', gen[row])
-        # print('---new--->', gen_next[row])
     # TODO: convert to generator with yield
     return gen_next
 
 
-def show_generations(current_generation=[], number_of_gen=1):
+def show_generations(current_generation=[], number_of_gen=1,
+                     ignore_borders=True):
     """
     Displays the specified number of generations
     """
     clear_screen()
     for current_gen_number in range(number_of_gen):
-        current_generation = next_generation(current_generation)
+        current_generation = next_generation(current_generation,
+                                             ignore_borders=ignore_borders)
         for line in current_generation:
             print(''.join(map(str, line))
                   .replace('0', '.')
                   .replace('1', 'O'))
         print('generation #', current_gen_number + 1)
-        time.sleep(0.5)
+        time.sleep(0.25)
         if current_gen_number != number_of_gen - 1:
             clear_screen()
             # TODO: how to clear screen in PyCharm terminal?
@@ -154,6 +126,23 @@ def str_2_list_playground(str_playground):
     return list(list_playground)
 
 
+def extend_playground(pattern, lines=0, columns=0,
+                      lines_before=0, cols_before=0):
+    """
+    Add given numbers of empty lines and empty columns before and after
+     every line and column of the playground
+    """
+    for line in range(len(pattern)):
+        pattern[line].extend([0 for _ in range(columns)])
+        pattern[line] = [0 for _ in range(cols_before)] + pattern[line]
+        if line == len(pattern) - 1:
+            pattern.extend([[0 for _ in range(len(pattern[line]))]
+                            for new_line in range(lines)])
+    pattern = make_playground(columns=len(pattern[0]),
+                              lines=lines_before) + pattern
+    return pattern
+
+
 def insert_pattern(playground, x, y):
     # TODO: insert some pattern into the playground from a given
     #   position (x, y) adding the missing pad sizes if needs
@@ -162,11 +151,40 @@ def insert_pattern(playground, x, y):
 
 if __name__ == "__main__":
     # TODO_done: move start generations in a file
-    # LWSS_gen = str_2_list_playground(LWSS_raw)
-    # show_generations(LWSS_gen, 100)
-    pulsar_gen = str_2_list_playground(pulsar_raw)
-    show_generations(pulsar_gen, 99)
+
+    # lwss_gen = str_2_list_playground(lwss_raw)
+    # lwss_gen = extend_playground(lwss_gen,lines=2, columns=10,
+    #                              cols_before=10)
+    # show_generations(lwss_gen, 100)
+
+    # pulsar_gen = str_2_list_playground(pulsar_raw)
+    # show_generations(pulsar_gen, 99)
+
     # glider_gun_gen_0 = str_2_list_playground(glider_gun_raw)
     # show_generations(glider_gun_gen_0, 500)
-    # gen_0 = scatter_life(make_playground(20, 5))
-    # show_generations(gen_0, 100)
+
+    # fuse_gen = str_2_list_playground(fuse_raw)
+    # fuse_gen = extend_playground(fuse_gen, columns=10, cols_before=10,
+    #                              lines=10, lines_before=10)
+    # show_generations(fuse_gen, 99)
+
+    # python_gen = str_2_list_playground(python_raw)
+    # python_gen = extend_playground(python_gen, 1, 1, 1, 1)
+    # show_generations(python_gen, 99)
+
+    # ff_force_field_gen = str_2_list_playground(ff_force_field_raw)
+    # ff_force_field_gen = extend_playground(ff_force_field_gen,
+    #                                        lines=5, columns=10,
+    #                                        lines_before=5,
+    #                                        cols_before=10)
+    # show_generations(ff_force_field_gen, 99, ignore_borders=True)
+
+    glider_gen = str_2_list_playground(glider_raw)
+    glider_gen = extend_playground(glider_gen,
+                                   lines=1, columns=1,
+                                   lines_before=1, cols_before=1)
+    show_generations(glider_gen, 99, ignore_borders=True)
+
+    gen_0 = scatter_life(make_playground(20, 5))
+    show_generations(gen_0, 100)
+
